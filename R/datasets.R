40c71b1fbd402ff4ca739ad765eae3fe24fc5def
Rob J Hyndman <robjhyndman@gmail.com>
2021-02-15 16:10:44 +1100

    Added wine data

--- R/datasets.R
+++ R/datasets.R
@@ -38,3 +38,25 @@
 #' }
 #' @source \url{http://geysertimes.org}
 "oldfaithful"
+
+#' Wine prices and points
+#'
+#' A data set containing data on wines from 44 countries, taken from *Wine Enthusiast Magazine*
+#' during the week of 15 June 2017.
+#'
+#' @format A data frame with 111,593 rows and 10 columns:
+#' \describe{
+#'   \item{country}{Country of origin}
+#'   \item{province}{Province of origin}
+#'   \item{region}{Region of origin}
+#'   \item{winery}{Name of vineyard that made the wine}
+#'   \item{variety}{Variety of grape}
+#'   \item{title}{Title of wine review}
+#'   \item{description}{A review of the wine}
+#'   \item{points}{Points allocated by WineEnthusiast reviewer on a scale of 0-100}
+#'   \item{price}{Price of a bottle of wine in $US}
+#'   \item{year}{Year of wine extracted from `title`}
+#' }
+#' @source \url{http://kaggle.com}
+"wine"
+
