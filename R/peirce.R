2ecfea4a4fb8f08fbb804882e77f7e5a1e2db335
Rob J Hyndman <robjhyndman@gmail.com>
2021-02-18 21:09:22 +1100

    Rewrote peirce_threshold and chauvenet_anomalies

--- R/peirce.R
+++ R/peirce.R
@@ -11,7 +11,7 @@
 #' @references Chauvenet, W. (1863). 'Method of least squares'. Appendix to
 #' *Manual of Spherical and Practical Astronomy*, Vol.2, Lippincott, Philadelphia, pp.469-566.
 #'
-#' @author Rob J Hyndman, K Thomsen, C Dardis, S Muller
+#' @author Rob J Hyndman
 #' @param y numerical vector of observations
 #' @examples
 #' y <- rnorm(1000)
@@ -24,35 +24,28 @@ peirce_anomalies <- function(y) {
   return(abs(z) > peirce_threshold(length(y)))
 }
 
-peirce_threshold <- function(n, k = 1L, m = 1L) {
-  # method by K. Thomsen (2008)
-  # written by C. Dardis and S. Muller (2012)
-  # modified by Rob J Hyndman (2021)
-  # Available online: https://r-forge.r-project.org/R/?group_id=1473
-
+# Threshold based on Gould's paper when there are m=1 unknown quantities
+# and n=1 suspicious observations
+peirce_threshold <- function(n) {
   # Check we have enough observations
-  if ((n - m - k) <= 0)
+  if ((n - 2) <= 0)
     return(NaN)
-
-  x <- min(1, sqrt((n - m) / k) - 1e-10)
-  # Log of Gould's equation B:
-  LnQN <- k * log(k) + (n - k) * log(n - k) - n * log(n)
-  # Gould's equation D:
-  R1 <- 2 * exp((x^2 - 1) / 2) * stats::pnorm(x, lower.tail = FALSE)
-  # Gould's equation A' solved for R w/ Lambda substitution:
-  R2 <- exp((LnQN - 0.5 * (n - k) * log((n - m - k * x^2) / (n - m - k))) / k)
-  # Equate the two R equations:
-  R1d <- x * R1 - sqrt(2 / pi / exp(1))
-  R2d <- x * (n - k) / (n - m - k * x^2) * R2
-  # Update x:
-  oldx <- x
-  x <- oldx - (R1 - R2) / (R1d - R2d)
-  # Loop until convergence:
-  while (abs(x - oldx) >= n * 2e-16) {
-    R1 <- 2 * exp((x^2 - 1) / 2) * stats::pnorm(x, lower.tail = FALSE)
-    R2 <- exp((LnQN - 0.5 * (n - k) * log((n - m - k * x^2) / (n - m - k))) / k)
-    R1d <- x * R1 - sqrt(2 / pi / exp(1))
-    R2d <- x * (n - k) / (n - m - k * x^2) * R2
+  # Initialize
+  x <- 1
+  oldx <- Inf
+  root2pie <- sqrt(2 / pi / exp(1))
+  # Eq (B) after taking logs
+  LnQN <- (n - 1) * log(n - 1) - n * log(n)
+  # Loop until convergence
+  while (abs(x - oldx) >= .Machine$double.eps) {
+    # Eq (D)
+    R1 <- 2 * exp(0.5*(x^2 - 1)) * stats::pnorm(x, lower.tail = FALSE)
+    # Eq (A') after taking logs and solving for R (plug in lambda from top of page)
+    R2 <- exp(LnQN - (n - 1) * 0.5 * log((n - 1 - x^2) / (n - 2)))
+    # Find derivatives wrt x
+    R1d <- x * R1 - root2pie
+    R2d <- x * (n - 1) / (n - 1 - x^2) * R2
+    # Update x accordingly
     oldx <- x
     x <- oldx - (R1 - R2) / (R1d - R2d)
   }
@@ -64,5 +57,5 @@ peirce_threshold <- function(n, k = 1L, m = 1L) {
 
 chauvenet_anomalies <- function(y) {
   z <- (y - mean(y, na.rm = TRUE)) / stats::sd(y, na.rm = TRUE)
-  2 * length(y) * stats::pnorm(abs(z) * sqrt(2), lower.tail = FALSE) <= 0.5
+  return(abs(z) > stats::qnorm(1-0.25/length(y)))
 }
