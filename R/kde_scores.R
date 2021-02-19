fb8b666af7fe5145b28ca2708bf8d3050e9e89cb
Rob J Hyndman <robjhyndman@gmail.com>
2021-02-19 15:41:26 +1100

    Added kde_scores

---
+++ R/kde_scores.R
@@ -0,0 +1,21 @@
+#' @title Kernel density estimate scores
+#' @description Compute kernel density estimate scores,
+#'  defined as minus the log of the kernel density estimate at each observation.
+#'  A Gaussian kernel is used, with default h given by a normal reference rule.
+#' @param y Numerical vector of data
+#' @param h Bandwidth. Dfault: \code{\link[stats]{bw.nrd}(y)}
+#' @param binned Should binning be used to speed up calculation? Default: FALSE
+#' @return Numerical vector containing kde scores
+#' @author Rob J Hyndman
+#' @examples
+#' kde_scores(c(rnorm(9),5))
+#' @export
+#' @rdname kde_scores
+#' @seealso
+#'  \code{\link[stats]{bandwidth}}
+#'  \code{\link[ks]{kde}}
+#' @importFrom stats bw.nrd
+#' @importFrom ks kde
+kde_scores <- function(y, h=stats::bw.nrd(y), binned=FALSE) {
+  -log(ks::kde(y, h = h,  binned=binned, eval.points = y)$estimate)
+}
