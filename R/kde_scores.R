c2dca1b00cee6b2b3cb12c188235c68c40fa32dd
Rob J Hyndman <robjhyndman@gmail.com>
2021-02-19 16:01:48 +1100

    Added loo argument to kde_scores

--- R/kde_scores.R
+++ R/kde_scores.R
@@ -5,6 +5,7 @@
 #' @param y Numerical vector of data
 #' @param h Bandwidth. Dfault: \code{\link[stats]{bw.nrd}(y)}
 #' @param binned Should binning be used to speed up calculation? Default: FALSE
+#' @param loo Should leave-one-scores be returned? Default: FALSE
 #' @return Numerical vector containing kde scores
 #' @author Rob J Hyndman
 #' @examples
@@ -16,6 +17,7 @@
 #'  \code{\link[ks]{kde}}
 #' @importFrom stats bw.nrd
 #' @importFrom ks kde
-kde_scores <- function(y, h=stats::bw.nrd(y), binned=FALSE) {
-  -log(ks::kde(y, h = h,  binned=binned, eval.points = y)$estimate)
+kde_scores <- function(y, h=stats::bw.nrd(y), binned=FALSE, loo=FALSE) {
+  -log(ks::kde(y, h = h,  binned=binned, eval.points = y)$estimate) -
+    log(length(y)*h*sqrt(2*pi)) * loo
 }
