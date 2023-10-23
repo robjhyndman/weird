449dc0969db7e958c196ca3c838291963d1f8cd3
Rob Hyndman <robjhyndman@gmail.com>
2023-10-23 17:00:58 +1100

    hdr test

---
+++ tests/testthat/test-hdr.R
@@ -0,0 +1,7 @@
+test_that("HDR", {
+  hdr <- hdr_table(rnorm(10000), prob = 0.95)
+  expect_equal(hdr$lower, -1.96, tolerance = 0.02)
+  expect_equal(hdr$upper, 1.96, tolerance = 0.02)
+  expect_equal(hdr$mode, 0, tolerance = 0.1)
+  expect_lt(abs(hdr$density - dnorm(1.96)), 0.01)
+})
