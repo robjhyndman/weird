739c99132d8dce3f3603d5fb9a7d43d226781104
Rob Hyndman <robjhyndman@gmail.com>
2023-11-07 14:36:11 +1100

    Extended kde_scores to multivariate data

--- R/kde_scores.R
+++ R/kde_scores.R
@@ -1,21 +1,26 @@
 #' @title Kernel density estimate scores and lookout probabilities
 #' @description Compute kernel density estimate scores,
 #'  defined as minus the log of the kernel density estimate at each observation.
-#'  A Gaussian kernel is used, with default h given by a normal reference rule.
+#'  A Gaussian kernel is used, with default bandwidth given by a normal reference rule.
 #'  Leave-one-out scores are optionally computed.
 #'  Lookout probabilities are returned by \code{lookout_prob} computed using a
 #'  Generalized Pareto Distribution estimated from the kde scores and applied
 #'  to the leave-one-out kde scores. These give the probability of each observation
 #'  being an anomaly.
 #' @param y Numerical vector or matrix of data (up to 6 dimensions).
+#' @param h Bandwidth for univariate kernel density estimate. Default is \code{\link[ks]{hns}}.
+#' @param H Bandwidth for multivariate kernel density estimate. Default is \code{\link[ks]{Hns}}.
 #' @param loo Should leave-one-scores be returned? Default: FALSE.
-#' @param h Bandwidth for the kernel density estimate. Default: normal reference rule given
-#' by \code{\link[stats]{bw.nrd0}}.
 #' @param ... Other arguments are passed to \code{\link[ks]{kde}}.
 #' @return Numerical vector containing kde scores.
 #' @author Rob J Hyndman
 #' @examples
-#' y <- c(rnorm(49), 5)
+#' # Univariate data
+#' y <- c(5, rnorm(49))
+#' kde_scores(y)
+#' lookout_prob(y)
+#' # Bivariate data
+#' y <- cbind(rnorm(50), c(5, rnorm(49)))
 #' kde_scores(y)
 #' lookout_prob(y)
 #' @export
@@ -23,10 +28,11 @@
 #' @seealso
 #'  \code{\link[stats]{bandwidth}}
 #'  \code{\link[ks]{kde}}
-#' @importFrom stats bw.nrd quantile density dnorm approx na.omit
+#' @importFrom stats quantile density dnorm approx na.omit
 #' @importFrom evd fpot pgpd
-kde_scores <- function(y, loo = FALSE, h = stats::bw.nrd0(y), ...) {
-  tmp <- calc_kde_scores(y, h = h, ...)
+#' @importFrom ks hns Hns
+kde_scores <- function(y, loo = FALSE, h = ks::hns(y), H = ks::Hns(y), ...) {
+  tmp <- calc_kde_scores(y, h, H, ...)
   if (loo) {
     return(tmp$loo_scores)
   } else {
@@ -37,8 +43,8 @@ kde_scores <- function(y, loo = FALSE, h = stats::bw.nrd0(y), ...) {
 #' @rdname kde_scores
 #' @export
 
-lookout_prob <- function(y, h = stats::bw.nrd0(y), ...) {
-  tmp <- calc_kde_scores(y, h = h, ...)
+lookout_prob <- function(y, loo = FALSE, h = ks::hns(y), H = ks::Hns(y), ...) {
+  tmp <- calc_kde_scores(y, h, H, ...)
   loo_scores <- tmp$loo_scores
   threshold <- stats::quantile(tmp$scores, prob = 0.90, type = 8)
   gpd <- evd::fpot(tmp$scores, threshold = threshold, std.err = FALSE)$estimate
@@ -48,16 +54,21 @@ lookout_prob <- function(y, h = stats::bw.nrd0(y), ...) {
   )
 }
 
-calc_kde_scores <- function(y, h, ...) {
-  if (NCOL(y) > 1) {
-    stop("Not yet implemented for multivariate data.")
+# Compute value of density at each observation using kde
+calc_kde_scores <- function(y, h = ks::hns(y), H = ks::Hns(y), ...) {
+  n <- NROW(y)
+  d <- NCOL(y)
+  # Estimate density at each observation
+  if(d == 1L) {
+    gridsize = 10001
+    K0 = 1/(h * sqrt(2 * pi))
+  } else {
+    gridsize = 101
+    K0 = det(H)^(-1/2) * (2*pi)^(-d/2)
   }
-  y <- na.omit(y)
-  n <- length(y)
-  fy <- ks::kde(y, h = h, eval.points = y, binned = n > 1000, ...)
-  fi <- fy$estimate
-  h <- fy$h
-  loo_scores <- -log(pmax(0, (n * fi - dnorm(0, 0, h)) / (n - 1)))
+  fi <- ks::kde(y, h = h, H = H, gridsize = gridsize, binned = n > 2000,
+      eval.points = y, compute.cont = FALSE, ...)$estimate
+  loo_scores <- -log(pmax(0, (n * fi - K0) / (n - 1)))
   scores <- -log(pmax(0, fi))
-  return(list(scores = scores, loo_scores = loo_scores, h = h))
+  return(list(scores = scores, loo_scores = loo_scores))
 }
