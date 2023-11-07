1228ec056798796e02a4f10f64524c231cca33e9
Rob Hyndman <robjhyndman@gmail.com>
2023-11-07 17:24:04 +1100

    Set seed in tests

--- tests/testthat/test-hdr.R
+++ tests/testthat/test-hdr.R
@@ -1,4 +1,5 @@
 test_that("HDR", {
+  set.seed(1)
   hdr <- hdr_table(rnorm(10000), prob = 0.95)
   expect_equal(hdr$lower, -1.96, tolerance = 0.05)
   expect_equal(hdr$upper, 1.96, tolerance = 0.05)
