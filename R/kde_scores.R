e313224f637d8fe2669b9486b65e982115b045c7
Rob J Hyndman <robjhyndman@gmail.com>
2023-11-09 14:58:39 +1100

    More robust kde_scores

--- R/kde_scores.R
+++ R/kde_scores.R
@@ -62,12 +62,14 @@ calc_kde_scores <- function(y, h = ks::hns(y), H = ks::Hns(y), ...) {
   if(d == 1L) {
     gridsize = 10001
     K0 = 1/(h * sqrt(2 * pi))
+    fi <- ks::kde(y, h = h, gridsize = gridsize, binned = n > 2000,
+            eval.points = y, compute.cont = FALSE, ...)$estimate
   } else {
     gridsize = 101
     K0 = det(H)^(-1/2) * (2*pi)^(-d/2)
+    fi <- ks::kde(y, H = H, gridsize = gridsize, binned = n > 2000,
+            eval.points = y, compute.cont = FALSE, ...)$estimate
   }
-  fi <- ks::kde(y, h = h, H = H, gridsize = gridsize, binned = n > 2000,
-      eval.points = y, compute.cont = FALSE, ...)$estimate
   loo_scores <- -log(pmax(0, (n * fi - K0) / (n - 1)))
   scores <- -log(pmax(0, fi))
   return(list(scores = scores, loo_scores = loo_scores))
