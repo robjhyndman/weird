d8a9bdebdcba60f46b8d3a76edadba8ca8c4fc00
Rob J Hyndman <robjhyndman@gmail.com>
2023-12-10 16:46:39 +1100

    Removed reg_scores and changed log_scores to density_scores

--- R/log_scores.R
+++ R/log_scores.R
@@ -1,27 +1,29 @@
-#' @title Log scores
-#' @description Compute log scores or leave-one-out log scores from a model or
-#'  a kernel density estimate of a data set.
-#'  The log scores are defined as minus the log of the conditional density,
-#'  or kernel density estimate, at each observation.
-#'  The leave-one-out log scores (or log LOO scores) are obtained by estimating the conditional density
-#'  or kernel density estimate using all other observations.
+#' @title Density scores
+#' @description Compute density scores or leave-one-out density scores from a
+#' model or a kernel density estimate of a data set.
+#' The density scores are defined as minus the log of the conditional density,
+#' or kernel density estimate, at each observation.
+#' The leave-one-out density scores (or LOO density scores) are obtained by
+#' estimating the conditional density or kernel density estimate using all
+#' other observations.
 #' @details If the first argument is a numerical vector or matrix, then
 #' a kernel density estimate is computed, using a Gaussian kernel,
 #' with default bandwidth given by a robust normal reference rule.
 #' Otherwise the model is used to compute the conditional
-#' density function at each observation, from which the log scores (or
-#' possibly the log LOO scores) are obtained.
+#' density function at each observation, from which the density scores (or
+#' possibly the LOO density scores) are obtained.
 #' @param object A model object or a numerical data set.
-#' @param loo Should leave-one-out log scores be computed?
+#' @param loo Should leave-one-out density scores be computed?
 #' @author Rob J Hyndman
-#' @return A numerical vector containing either the log scores, or the log LOO scores.
+#' @return A numerical vector containing either the density scores, or the LOO
+#' density scores.
 #' @export
 
-log_scores <- function(object, loo = FALSE, ...) {
-  UseMethod("log_scores")
+density_scores <- function(object, loo = FALSE, ...) {
+  UseMethod("density_scores")
 }
 
-#' @rdname log_scores
+#' @rdname density_scores
 #' @param h Bandwidth for univariate kernel density estimate. Default is \code{\link{kde_bandwidth}}.
 #' @param H Bandwidth for multivariate kernel density estimate. Default is \code{\link{kde_bandwidth}}.
 #' @param ... Other arguments are passed to \code{\link[ks]{kde}}.
@@ -29,9 +31,9 @@ log_scores <- function(object, loo = FALSE, ...) {
 #' of <- oldfaithful |>
 #'   filter(duration < 7000, waiting < 7000) |>
 #'   mutate(
-#'     logscores = log_scores(cbind(duration, waiting)),
-#'     loglooscores = log_scores(cbind(duration, waiting), loo = TRUE),
-#'     lookout_prob = lookout(logscores, loglooscores)
+#'     fscores = density_scores(cbind(duration, waiting)),
+#'     loo_fscores = density_scores(cbind(duration, waiting), loo = TRUE),
+#'     lookout_prob = lookout(fscores, loo_fscores)
 #'   )
 #' of |>
 #'   ggplot(aes(x = duration, y = waiting, color = lookout_prob < 0.01)) +
@@ -40,9 +42,8 @@ log_scores <- function(object, loo = FALSE, ...) {
 #'  \code{\link{kde_bandwidth}}
 #'  \code{\link[ks]{kde}}
 #' @export
-log_scores.default <- function(object, loo = FALSE,
-                               h = kde_bandwidth(object), H = kde_bandwidth(object),
-                               ...) {
+density_scores.default <- function(object, loo = FALSE,
+   h = kde_bandwidth(object), H = kde_bandwidth(object), ...) {
   object <- as.matrix(object)
   tmp <- calc_kde_scores(object, h, H, ...)
   if (loo) {
@@ -52,7 +53,7 @@ log_scores.default <- function(object, loo = FALSE,
   }
 }
 
-#' @rdname log_scores
+#' @rdname density_scores
 #' @param ... Other arguments are ignored.
 #' @examples
 #' of <- oldfaithful |>
@@ -60,9 +61,9 @@ log_scores.default <- function(object, loo = FALSE,
 #' f_kde <- kde(of[,2:3], H = kde_bandwidth(of[,2:3]))
 #' of |>
 #'   mutate(
-#'     logscores = log_scores(f_kde),
-#'     loglooscores = log_scores(f_kde, loo = TRUE),
-#'     lookout_prob = lookout(logscores, loglooscores)
+#'     fscores = density_scores(f_kde),
+#'     loo_fscores = density_scores(f_kde, loo = TRUE),
+#'     lookout_prob = lookout(fscores, loo_fscores)
 #'   ) |>
 #'   ggplot(aes(x = duration, y = waiting, color = lookout_prob < 0.01)) +
 #'   geom_point()
@@ -70,7 +71,7 @@ log_scores.default <- function(object, loo = FALSE,
 #'  \code{\link{kde_bandwidth}}
 #'  \code{\link[ks]{kde}}
 #' @export
-log_scores.kde <- function(object, loo = FALSE, ...) {
+density_scores.kde <- function(object, loo = FALSE, ...) {
   n <- NROW(object$x)
   d <- NCOL(object$x)
   # Estimate density at each observation using interpolation
@@ -97,59 +98,85 @@ log_scores.kde <- function(object, loo = FALSE, ...) {
   }
 }
 
-#' @rdname log_scores
+#' @rdname density_scores
 #' @examples
 #' shiraz <- wine_reviews |> filter(variety %in% c("Shiraz", "Syrah"))
 #' fit_wine <- lm(log(price) ~ points, data = shiraz)
 #' shiraz |>
 #'   mutate(
-#'     logscore = log_scores(fit_wine),
-#'     loglooscore = log_scores(fit_wine, loo = TRUE),
-#'     lookout_prob = lookout(logscore, loglooscore)
+#'     fscore = density_scores(fit_wine),
+#'     loo_fscore = density_scores(fit_wine, loo = TRUE),
+#'     lookout_prob = lookout(fscore, loo_fscore)
 #'   ) |>
 #'   ggplot(aes(x = points, y = price, color = lookout_prob < 0.02)) +
 #'   geom_jitter(height = 0, width = 0.2) +
 #'   scale_y_log10()
 #' @export
-log_scores.lm <- function(object, loo = FALSE, ...) {
-  fit_aug <- broom::augment(object)
-  if(loo) {
-    fit_aug$.std.resid <- fit_aug$.resid / (fit_aug$.sigma * sqrt(1 - fit_aug$.hat))
+density_scores.lm <- function(object, loo = FALSE, ...) {
+  e <- stats::residuals(object, type = "response")
+  h <- stats::hatvalues(object)
+  sigma2 <- sum(e^2, na.rm = TRUE)/object$df.residual
+  if (loo) {
+    resdf <- object$df.residual
+    sigma2 <- (sigma2 * resdf - e^2/(1-h)) / (resdf - 1)
   }
-  return(-dnorm(fit_aug$.std.resid, log = TRUE))
+  r2 <- e^2 / ((1-h) * sigma2)
+  return(0.5 * (log(2*pi) + r2))
 }
 
-#' @rdname log_scores
+
+#' @rdname density_scores
 #' @examples
 #' shiraz <- wine_reviews |> filter(variety %in% c("Shiraz", "Syrah"))
 #' fit_wine <- mgcv::gam(log(price) ~ s(points), data = shiraz)
 #' shiraz |>
 #'   mutate(
-#'     logscore = log_scores(fit_wine),
-#'     lookout_prob = lookout(logscore)
+#'     fscore = density_scores(fit_wine),
+#'     lookout_prob = lookout(fscore)
 #'   ) |>
 #'   ggplot(aes(x = points, y = price, color = lookout_prob < 0.02)) +
 #'   geom_jitter(height = 0, width = 0.2) +
 #'   scale_y_log10()
 #' @importFrom stats approx dbinom density dnorm dpois na.omit
 #' @export
-log_scores.gam <- function(object, loo = FALSE, ...) {
+density_scores.gam <- function(object, loo = FALSE, ...) {
   if(loo) {
-    warning("Leave-one-out scores unavailable for GAM models")
+    warning("Leave-one-out log scores unavailable for GAM models. Returning log scores.")
   }
   fit_aug <- broom::augment(object, type.predict = "response")
   if(object$family$family == "gaussian") {
     std.resid <- c(scale(fit_aug$.resid / fit_aug$.se.fit))
-    log_scores <- -dnorm(std.resid, log = TRUE)
+    density_scores <- -dnorm(std.resid, log = TRUE)
   } else if(object$family$family == "binomial") {
-    log_scores <- -dbinom(x = object$y*object$prior.weights,
+    density_scores <- -dbinom(x = object$y*object$prior.weights,
                           size = object$prior.weights, prob = fit_aug$.fitted, log = TRUE)
   } else if(object$family$family == "poisson") {
-    log_scores <- -dpois(object$y, lambda = fit_aug$.fitted, log = TRUE)
+    density_scores <- -dpois(object$y, lambda = fit_aug$.fitted, log = TRUE)
   } else {
     stop("Unsupported family")
   }
-  return(log_scores)
+  return(density_scores)
+}
+
+# Compute value of density at each observation using kde
+calc_kde_scores <- function(y, h = kde_bandwidth(y), H = kde_bandwidth(y), ...) {
+  n <- NROW(y)
+  d <- NCOL(y)
+  # Estimate density at each observation
+  if(d == 1L) {
+    gridsize = 10001
+    K0 = 1/(h * sqrt(2 * pi))
+    fi <- ks::kde(y, h = h, gridsize = gridsize, binned = n > 2000,
+                  eval.points = y, compute.cont = FALSE, ...)$estimate
+  } else {
+    gridsize = 101
+    K0 = det(H)^(-1/2) * (2*pi)^(-d/2)
+    fi <- ks::kde(y, H = H, gridsize = gridsize, binned = n > 2000,
+                  eval.points = y, compute.cont = FALSE, ...)$estimate
+  }
+  loo_scores <- -log(pmax(0, (n * fi - K0) / (n - 1)))
+  scores <- -log(pmax(0, fi))
+  return(list(scores = scores, loo_scores = loo_scores))
 }
 
 utils::globalVariables(c(".resid",".se.fit",".std.resid",".resid",".sigma",".hat",
--- R/lookout.R
+++ R/lookout.R
@@ -4,7 +4,7 @@
 #' being an anomaly.
 #' @details If the log_loo_scores are not available, the log scores are used
 #' to compute the anomaly probabilities.
-#' @param log_scores Numerical vector of log scores
+#' @param density_scores Numerical vector of log scores
 #' @param log_loo_scores Optional numerical vector of leave-one-out log scores
 #' @param threshold_probability Probability threshold when computing the POT model for the log scores.
 #' @references Sevvandi Kandanaarachchi & Rob J Hyndman (2022) "Leave-one-out
@@ -16,26 +16,26 @@
 #' # Univariate data
 #' tibble(
 #'   y = c(5, rnorm(49)),
-#'   logscores = log_scores(y),
-#'   loglooscores = log_scores(y, loo = TRUE),
-#'   lookout = lookout(logscores, loglooscores)
+#'   fscores = density_scores(y),
+#'   loo_fscores = density_scores(y, loo = TRUE),
+#'   lookout = lookout(fscores, loo_fscores)
 #' )
 #' # Bivariate data
 #' tibble(
 #'   x = rnorm(50),
 #'   y = c(5, rnorm(49)),
-#'   logscores = log_scores(y),
-#'   loglooscores = log_scores(y, loo = TRUE),
-#'   lookout = lookout(logscores, loglooscores)
+#'   fscores = density_scores(y),
+#'   loo_fscores = density_scores(y, loo = TRUE),
+#'   lookout = lookout(fscores, loo_fscores)
 #' )
 #' # Using a regression model
 #' shiraz <- wine_reviews |> filter(variety %in% c("Shiraz", "Syrah"))
 #' fit_wine <- lm(log(price) ~ points, data = shiraz)
 #' shiraz |>
 #'   mutate(
-#'     logscore = log_scores(fit_wine),
-#'     loglooscore = log_scores(fit_wine, loo = TRUE),
-#'     lookout_prob = lookout(logscore, loglooscore)
+#'     fscore = density_scores(fit_wine),
+#'     loo_fscore = density_scores(fit_wine, loo = TRUE),
+#'     lookout_prob = lookout(fscore, loo_fscore)
 #'   ) |>
 #'   arrange(lookout_prob)
 #' @importFrom stats quantile
@@ -43,9 +43,9 @@
 
 #' @export
 
-lookout <- function(log_scores, log_loo_scores = log_scores, threshold_probability = 0.95) {
-  threshold <- stats::quantile(log_scores, prob = threshold_probability, type = 8)
-  gpd <- evd::fpot(log_scores, threshold = threshold, std.err = FALSE)$estimate
+lookout <- function(density_scores, log_loo_scores = density_scores, threshold_probability = 0.95) {
+  threshold <- stats::quantile(density_scores, prob = threshold_probability, type = 8)
+  gpd <- evd::fpot(density_scores, threshold = threshold, std.err = FALSE)$estimate
   lookout_probabilities <- evd::pgpd(
     log_loo_scores, loc = threshold,
     scale = gpd["scale"], shape = gpd["shape"], lower.tail = FALSE
