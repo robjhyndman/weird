26d8b19ea43a7063c37327fa5e45816fbaea5516
Rob J Hyndman <robjhyndman@gmail.com>
2023-12-15 13:00:03 +1100

    Refactored kde_bandwidth and added iterative bandwidth selection

--- R/density.R
+++ R/density.R
@@ -136,32 +136,52 @@ density_on_grid <- function(y, fy, ngrid) {
 #' @param method Method to use for selecting the bandwidth.
 #' `robust_normal` uses a robust version of the normal reference rule.
 #' `lookout` uses the topological data analysis approach that is part of the lookout algorithm.
+#' @param iterate Should the `lookout` method be iterated. That is, outliers
+#' (probability < 0.05) are removed and the bandwidth is re-computed from the
+#' remaining observations. Ignored if `method="robust_normal"`.
 #' @return A matrix of bandwidths (or scalar in the case of univariate data).
 #' @author Rob J Hyndman
 #' @export
 
-kde_bandwidth <- function(data, method = c("robust_normal", "lookout")) {
+kde_bandwidth <- function(data, method = c("robust_normal", "lookout"),
+                          max.iter = 2) {
   method <- match.arg(method)
   d <- NCOL(data)
   n <- NROW(data)
-  if(d == 1L) {
-    # Find robust scale of data
-    if(method == "robust_normal") {
-      return(1.06 * robustbase::s_IQR(data) * n^(-0.2))
-    } else {
-      return(lookout::find_tda_bw(data, fast = (n > 1000)))
-    }
-  } else {
+  if(d == 2) {
     # Find robust covariance matrix of data
     S <- robustbase::covOGK(data, sigmamu = robustbase::s_IQR)$cov
-    if(method == "robust_normal") {
+  }
+  if(method == "robust_normal") {
+    if(d == 1L)
+      return(1.06 * robustbase::s_IQR(data) * n^(-0.2))
+    else {
       return((4/(n * (d + 2)))^(2/(d + 4)) * S)
-    } else {
-      # Computer h* from normalized data
-      U <- chol(solve(S))
-      hstar <- lookout::find_tda_bw(as.matrix(data) %*% t(U), fast = (n > 1000))
-      return(hstar * S)
     }
+  } else {
+    # Initial estimate
+    if(d == 1L) {
+      S <- 1
+    } else {
+      # Normalize data
+      U <- chol(solve(S))
+      data <- as.matrix(data) %*% t(U)
+    }
+    h <- qnorm(0.99)*lookout::find_tda_bw(data, fast = (n > 1000)) |>
+      suppressWarnings()
+    iter <- 1
+    oldh <- 0
+    while(iter < max.iter & h != oldh) {
+      iter <- iter + 1
+      oldh <- h
+      scores <- calc_kde_scores(data, h=h, H=h*diag(d))
+      p <- lookout(density_scores = scores$scores, loo_scores = scores$loo) |>
+        suppressWarnings()
+      data <- as.matrix(data)[p > 0.05,]
+      # Refined estimate
+      h <- qnorm(0.99)*lookout::find_tda_bw(data, fast = (n > 1000))
+    }
+    return(h * S)
   }
 }
 
