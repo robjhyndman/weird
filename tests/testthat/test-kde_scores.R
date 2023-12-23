5185c0ef526e375e5c6c3b1686f8d01121bda19d
Rob Hyndman <robjhyndman@gmail.com>
2023-12-23 17:24:56 +1100

    Removed stray comma

--- tests/testthat/test-kde_scores.R
+++ tests/testthat/test-kde_scores.R
@@ -2,6 +2,6 @@ test_that("multiplication works", {
   y <- c(rnorm(10), 100)
   expect_equal(
     max(density_scores(y, h = 1)),
-    log(11 / dnorm(0, 0, 1)),
+    log(11 / dnorm(0, 0, 1))
   )
 })
