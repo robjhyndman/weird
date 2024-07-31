ca6dbdf03a17a4b85ec5455078f490c1cb13ae49
Rob Hyndman <robjhyndman@gmail.com>
2024-07-31 12:17:20 +1000

    Added dist_kde tests

---
+++ tests/testthat/test_dist_kde1.R
@@ -0,0 +1,31 @@
+# Univariate kde tests
+test_that("dist_kde1", {
+  set.seed(123)
+  x <- rnorm(100)
+  y <- c(rnorm(100), rnorm(100,5))
+  dist <- dist_kde(list(x, y))
+  # Mean
+  expect_equal(mean(dist), c(mean(x), mean(y)))
+  # Median
+  expect_equal(median(dist), quantile(dist, 0.5))
+  # Variance
+  expect_equal(distributional::variance(dist) > 0, c(TRUE,TRUE))
+  # Density
+  at <- seq(-4, 10, by = 1)
+  expect_equal(lengths(density(dist, at)), c(15L, 15L))
+  # CDF
+  expect_equal(lengths(distributional::cdf(dist, at)), c(15L, 15L))
+  # Quantiles
+  p <- (1:19) / 20
+  expect_equal(lengths(quantile(dist, p = p)), c(19L, 19L))
+  # Generate
+  rand_dist <- distributional::generate(dist, times = 1e6)
+  expect_equal(lapply(rand_dist, mean) |> unlist(),
+    mean(dist),
+    tolerance = 0.005
+  )
+  expect_equal(lapply(rand_dist, var) |> unlist(),
+    distributional::variance(dist),
+    tolerance = 0.005
+  )
+})
---
+++ tests/testthat/test_dist_kde2.R
@@ -0,0 +1,28 @@
+# Univariate kde tests
+test_that("dist_kde2", {
+  set.seed(123)
+  x <- rnorm(200)
+  y <- c(rnorm(100), rnorm(100,5))
+  dist <- dist_kde(cbind(x, y))
+  # Mean
+  expect_equal(mean(dist), c(x=mean(x), y=mean(y)))
+  # Median
+  expect_error(median(dist))
+  # Variance
+  expect_error(distributional::covariance(dist))
+  # Density
+  at <- expand.grid(x = seq(-3,3,by=0.5), y = seq(-2, 10, by = 2)) |>
+    as.matrix()
+  expect_equal(all(density(dist, at)[[1]] > 0), TRUE)
+  # CDF
+  expect_error(distributional::cdf(dist, at))
+  # Quantiles
+  p <- (1:19) / 20
+  expect_error(quantile(dist, p))
+  # Generate
+  rand_dist <- distributional::generate(dist, times = 1e6)
+  expect_equal(lapply(rand_dist, colMeans) |> unlist(),
+    mean(dist),
+    tolerance = 0.005
+  )
+})
