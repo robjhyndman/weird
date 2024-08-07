694cf61521d282b388b9a91ea581e324c4df71b4
Rob Hyndman <robjhyndman@gmail.com>
2024-08-07 15:24:15 +1000

    surprisal_prob() now calling surprisals()

--- R/surprisal_prob.R
+++ R/surprisal_prob.R
@@ -1,57 +1,61 @@
 #' Surprisal probabilities
 #'
-#' Compute the probability of a surprisal at least as extreme as `g`.
-#' A surprisal is given by \eqn{g = -\log(f)}
-#' where \eqn{f} is the density or probability mass function of the
-#' distribution.
+#' Compute the probability of a surprisal at least as extreme as those observed.
+#' A surprisal is given by \eqn{-\log(f)} where \eqn{f} is the density or
+#' probability mass function of the distribution.
 #' These probabilities may be computed in three different ways.
-#' 1. Given a specified `distribution`.
+#' 1. Given a specified `distribution` passed as an argument.
 #' 2. Using a Generalized Pareto Distribution fitted to the most extreme
-#' values of `g` (those above the `threshold_probability` quantile). This
-#' option is used if `GPD = TRUE`. For `g` values with tail probability
-#' greater than `threshold_probability`, the value of `threshold_probability`
-#' is returned.
-#' 3. Empirically as the proportion of values above `g`. This option is
-#' used when `GPD = FALSE` and `distribution = NULL`.
+#' surprisal values (those with probability less than the `threshold_probability`
+#' quantile). This option is used if `GPD = TRUE`. For surprisal values with
+#' tail probability less than `threshold_probability`, the value of
+#' `threshold_probability` is returned.
+#' 3. Empirically as the proportion of values with greater surprisal values.
+#' This option is used when `GPD = FALSE` and no distribution is provided.
 #'
-#' @param g vector of surprisals.
-#' @param distribution A distributional object specifying the probability
-#' distribution to use.
+#' @param object A model or numerical data set.
+#' @param distribution A probability distribution stored as a distributional
+#' object. Ignored if `object` is a model.
 #' @param GPD Logical value specifying if a Generalized Pareto distribution
-#' should be used to estimate the tail probabilities.
-#' @param smallest_prob Smallest detectable tail probability to be used
-#' if empirical tail proportions are used. If the surprisal has lower probability
+#' should be used to estimate the probabilities.
+#' @param smallest_prob Smallest detectable probability to be used
+#' if empirical proportions are used. If the surprisal has lower probability
 #' than this, `smallest_prob` is returned.
-#' @param gridsize Size of grid used in estimating the empirical tail
-#' probabilities. A larger number gives more accurate estimates but takes
-#' more time.
+#' @param gridsize Size of grid used in estimating the empirical probabilities.
+#' A larger number gives more accurate estimates but takes more time.
 #' @param threshold_probability Probability threshold when computing the GPD
 #' distribution for the surprisals.
+#' @param ... Other arguments are passed to \code{\link{surprisals}}.
 #' @examples
-#' surprisal_prob(-dnorm(1:3, log = TRUE), dist_normal())
+#' surprisal_prob(-3:3, dist_normal())
 #' tibble(
 #'   y = n01$v1,
-#'   g = -dnorm(y, log = TRUE),
-#'   prob1 = surprisal_prob(g, dist_normal()),
-#'   prob2 = surprisal_prob(g, GPD = TRUE),
-#'   prob3 = surprisal_prob(g)
+#'   prob1 = surprisal_prob(y, dist_normal()),
+#'   prob2 = surprisal_prob(y, GPD = TRUE),
+#'   prob3 = surprisal_prob(y)
 #' ) |>
 #'   filter(prob1 < 0.01 | prob2 < 0.01 | prob3 < 0.01)
 
 #' @export
 
-surprisal_prob <- function(
-    g,
-    distribution = NULL, GPD = FALSE,
+surprisal_prob <- function(object,
+    distribution = NULL,
+    GPD = FALSE,
     smallest_prob = 1e-6, gridsize = 100001,
-    threshold_probability = 0.90) {
+    threshold_probability = 0.10,
+    ...) {
+  if(is.null(distribution)) {
+    g <- surprisals(object)
+  } else {
+    g <- surprisals(object, distribution = distribution, ...)
+  }
   n <- length(g)
   if (GPD & !is.null(distribution)) {
     warning("GPD is specified, so the distribution argument will be ignored.")
   }
   if (GPD) {
     threshold <- stats::quantile(g,
-      prob = threshold_probability,
+      prob = 1-threshold_probability,
       type = 8, na.rm = TRUE
     )
     if (!any(g > threshold, na.rm = TRUE)) {
@@ -63,7 +67,7 @@ surprisal_prob <- function(
       warning("Infinite surprisals will be ignored in GPD.")
     }
     gpd <- evd::fpot(g[finite], threshold = threshold, std.err = FALSE)$estimate
-    p <- (1 - threshold_probability) * evd::pgpd(
+    p <- threshold_probability * evd::pgpd(
       g,
       loc = threshold,
       scale = gpd["scale"], shape = gpd["shape"], lower.tail = FALSE
