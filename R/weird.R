aeda83fd02f4642b199cb3e73f0055b23f30ccfb
Rob J Hyndman <robjhyndman@gmail.com>
2025-09-18 16:52:38 +1000

    reexport mvscale

--- R/weird.R
+++ R/weird.R
@@ -8,5 +8,8 @@
 #' @importFrom evd fpot pgpd
 #' @importFrom ggplot2 autoplot labs ggplot geom_line geom_segment
 #' @importFrom ggplot2 geom_contour_filled geom_contour scale_fill_manual
+#' @importFrom lookout mvscale
 #' @export
 ggplot2::autoplot
+#' @export
+lookout::mvscale
