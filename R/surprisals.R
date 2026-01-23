14ee3dfdcbb5abf83f1a07264a89b3a44fc0565e
Rob J Hyndman <robjhyndman@gmail.com>
2026-01-23 15:28:32 +1100

    Refining docs

--- R/surprisals.R
+++ R/surprisals.R
@@ -7,23 +7,26 @@
 #' as \eqn{s}. This is returned by `surprisals_prob()`
 #'
 #' @details The surprisal probabilities may be computed in three different ways.
-#' 1. Given the same distribution that was used to compute the surprisal values
-#' (when `approximation = "none"`).
+#' 1. When `approximation = "none"`, the surprisal probabilities are computed
+#' using the same distribution that was used to compute the surprisal values.
 #' Under this option, surprisal probabilities are equal to 1 minus the
 #' coverage probability of the largest HDR that contains each value. Surprisal
 #' probabilities smaller than 1e-6 are returned as 1e-6.
-#' 2. Using a Generalized Pareto Distribution fitted to the most extreme
+#' 2. When `approximation = "gdp"` (the default), the surprisal probabilities are
+#' computed using a Generalized Pareto Distribution fitted to the most extreme
 #' surprisal values (those with probability less than `threshold_probability`).
-#' This option is used when `approximation = "gdp"`, and is the default.
 #' For surprisal probabilities greater than `threshold_probability`, the value of
 #' `threshold_probability` is returned. Under this option, the distribution is
 #' used for computing the surprisal values but not for determining their
 #' probabilities. Due to extreme value theory, the resulting probabilities should
 #' be relatively insensitive to the distribution used in computing the surprisal
 #' values.
-#' 3. Using ranks (`approximation = "rank"`), the approximate probability is
-#' the proportion of observations with greater surprisal values.
-#' This is also insensitive to the distribution used in computing the surprisal values.
+#' 3. When `approximation = "rank"`, the surprisal probability of each
+#' observation is estimated using the proportion of observations with
+#' greater surprisal values; i.e., 1 - rank(s)/n where `rank(s)` is the rank
+#' of the surprisal value `s` among all surprisal values, and `n` is the
+#' number of observations. This is a nonparametric approach that is also
+#' insensitive to the distribution used in computing the surprisal values.
 #' @param object A model or numerical data set
 #' @param approximation Character string specifying the method to use in
 #' computing the surprisal probabilities. See Details below.
