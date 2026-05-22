959c838c954892c031108e331f761bf8452f8a60
Rob J Hyndman <robjhyndman@gmail.com>
2026-05-22 11:15:41 +1000

    dist_mclust now working for univariate and multivariate examples

--- R/gmm.R
+++ R/gmm.R
@@ -3,31 +3,46 @@
 #' @param object Object of class `Mclust`, output from the [[mclust::Mclust]] function
 #' @return An object of class `distributional`
 #' @examples
-#' \dontrun{
 #' library(mclust)
-#' # Create a bivariate Gaussian mixture model for the Old Faithful data
-#' gmm <- oldfaithful |>
-#'   select(duration, waiting) |>
-#'   Mclust() |>
-#'   gmm_to_dist()
+#' # Univariate mixture density
+#' gmm <- Mclust(oldfaithful$duration) |> dist_mclust()
 #' gg_density(gmm) +
-#'   geom_point(data = oldfaithful, aes(x = duration, y = waiting), alpha = 0.1) +
-#'   labs(x = "Duration", y = "Waiting time")
-#' }
+#'   geom_jitter(data = oldfaithful, aes(x=duration, y = -0.0002),
+#'     width=0, height=0.0002, alpha = 0.1) +
+#'   labs(x = "Eruption duration", y = "")
 #' @export
 dist_mclust <- function(object) {
   if (!requireNamespace("mclust", quietly = TRUE)) {
-    stop("Package 'mclust' is required to use this function.")
+    stop("Package 'mclust' is required to use this function")
   }
   if (!inherits(object, "Mclust")) {
-    stop("Object must be of class 'Mclust'.")
+    stop("Object must be of class 'Mclust'")
   }
   par <- object$parameters
+  equal_var <- par$variance$modelName == "E"
+  mean <- unname(par$mean)
+  if (is.matrix(mean)) {
+    sigma <- unname(par$variance$sigma)
+  } else {
+    sigma <- sqrt(par$variance$sigmasq)
+    if (equal_var) {
+      sigma <- rep(sigma, object$G)
+    }
+  }
+
   dist_list <- lapply(seq_len(object$G), function(i) {
-    distributional::dist_multivariate_normal(
-      list(unname(par$mean[, i])),
-      list(unname(par$variance$sigma[,, i]))
-    )
+    if (is.matrix(mean)) {
+      distributional::dist_multivariate_normal(
+        list(mean[, i]),
+        list(sigma[,, i])
+      )
+    } else {
+      distributional::dist_normal(par$mean[i], sigma[i])
+    }
   })
-  do.call(distributional::dist_mixture, c(dist_list, list(weights = par$pro)))
+  if (object$G == 1L) {
+    return(dist_list[[1]])
+  } else {
+    do.call(distributional::dist_mixture, c(dist_list, list(weights = par$pro)))
+  }
 }
