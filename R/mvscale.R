2b89c850ca96119b9f259439b37e12d005509ad7
Torben Tvedebrink <totvi@vestas.com>
2026-06-03 14:28:34 +0200

    mvscale optionally returns center and scaling terms

--- R/mvscale.R
+++ R/mvscale.R
@@ -26,6 +26,9 @@
 #' @param cov A function to compute the covariance matrix. Set to NULL if no rotation required. `cov()` must either return the matrix directly, or a list containing a matrix named `cov`.
 #' @param alpha When `cov = robustbase::covMcd()`, `alpha` controls the size of the subsets over which the determinant is minimized. Otherwise it is ignored. Set to 0.9 by default.
 #' @param warning Should a warning be issued if non-numeric columns are ignored?
+#' @param what What to return? Default `"object"` returns the scaled data.
+#' Alternatively, `"terms"` returns the location and scale/covariance estimates,
+#' while `"all"` gives both the scaled data and estimated quantities.
 #' @param ... Other arguments are passed to `cov()`.
 #' @return A vector, matrix or data frame of the same size and class as `object`,
 #' but with numerical variables replaced by scaled versions (renamed if they have been rotated).
@@ -51,8 +54,15 @@ mvscale <- function(
   cov = robustbase::covMcd,
   alpha = 0.9,
   warning = TRUE,
+  what = c("object", "terms", "all"),
   ...
 ) {
+  what <- match.arg(what)
+  what_terms <- FALSE
+  if(what %in% c("terms", "all")){
+    terms_list <- list()
+    what_terms <- TRUE
+  }
   d <- NCOL(object)
   # Check if input is a vector
   is_vec <- d == 1L &&
@@ -91,6 +101,7 @@ mvscale <- function(
   if (!is.null(center)) {
     med <- apply(mat, 2, \(x) center(na.omit(x)))
     mat <- sweep(mat, 2L, med)
+    if(what_terms) terms_list <- c(terms_list, list("center" = med))
   }
   # scale function that ignores missing values
   if (!is.null(scale)) {
@@ -102,7 +113,12 @@ mvscale <- function(
   if (d == 1L) {
     z <- mat / my_scale(mat)
     if (is_vec) {
-      return(as.vector(z))
+      if(what_terms){
+        terms_list <- c(terms_list, list("scale" = my_scale(mat)))
+        if(what == "all") return(c(list("object" = as.vector(z)), terms_list))
+        return(terms_list)
+      }
+      else return(as.vector(z))
     }
   } else if (!is.null(cov)) {
     # Compute covariance matrix from non-missing values
@@ -135,6 +151,7 @@ mvscale <- function(
       }
       warning("Covariance matrix is singular. Adding a small ridge penalty.")
     }
+    if(what_terms) terms_list <- c(terms_list, list("cov" = S, "cov_inverse" = Sinv))
     # Compute scaled and rotated data
     U <- chol(Sinv)
     z <- mat %*% t(U)
@@ -142,7 +159,9 @@ mvscale <- function(
     # Just scale, no rotation
     s <- apply(mat, 2, my_scale)
     z <- sweep(mat, 2L, s, "/")
+    if(what_terms) terms_list <- c(terms_list, list("scale" = s))
   }
+  if(what == "terms") return(terms_list)
   # Convert back to matrix, data frame or tibble if necessary
   idx <- which(numeric_col)
   for (i in seq_along(idx)) {
@@ -152,5 +171,6 @@ mvscale <- function(
   if (!is.null(cov)) {
     names(object)[numeric_col] <- paste0("z", seq(sum(numeric_col)))
   }
+  if(what == "all") return(c(list("object" = object), terms_list))
   return(object)
 }
